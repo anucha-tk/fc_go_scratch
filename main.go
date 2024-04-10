2946d1cbdd2c3030482b84bae485c01684727083
Anucha.tk <anuchataowkaen@gmail.com>
2024-04-10 22:21:08 +0700

    simple http wiht chi router

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT not found")
+	}
+
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+	router.Use(middleware.Logger)
+	v1Router := chi.NewRouter()
+	v1Router.Get("/ready", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+	router.Mount("/v1", v1Router)
+
+	srv := http.Server{
+		Addr:    "localhost:" + port,
+		Handler: router,
+	}
+	fmt.Println("Server started on port:", port)
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal("Server error:", err)
+	}
+}
